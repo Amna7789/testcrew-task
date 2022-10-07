5f0708ba395a98b8c65ebbf23d9ec158457e53a6
amna7789 <amnaaltaf242@gmail.com>
2022-10-07 20:15:01 +0300

    task 8 has finalized

---
+++ src/test/java/com/example/testing/AutoSuggestDropDown.java
@@ -0,0 +1,80 @@
+package com.example.testing;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.asynchttpclient.util.Assertions;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.List;
+
+public class AutoSuggestDropDown {
+
+    WebDriver driver;
+
+    @BeforeTest
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    void testCase1() throws Exception {
+        String URL = "https://www.google.com/webhp?hl=en";
+
+        driver.get(URL);
+       // driver.navigate().to("http://www.google.com/");
+
+        // Click on the search text box and send value
+        driver.findElement(By.name("q")).sendKeys("java tutorials");
+
+        List<WebElement> dropDownList = driver.findElements(By.xpath("//*[@class='sbct']"));
+
+        for (WebElement webElement : dropDownList) {
+            System.out.println(webElement.getText());
+        }
+
+        // Click on the search button
+        driver.findElement(By.name("btnK")).submit();
+
+        List<WebElement> selectedList = driver.findElements(By.xpath("//h3[contains(., 'W3Schools')]"));
+
+        Assert.assertNotEquals(0, selectedList.size(), "Result does not have W3Schools");
+
+        selectedList.get(0).click();
+
+        takeSnapShot(driver, ".\\screenshot\\suggestDD.jpg");
+    }
+
+    @AfterTest
+    void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000L);
+        driver.close();
+    }
+
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+        //Convert web driver object to TakeScreenshot
+
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Move image file to new destination
+
+        File DestFile=new File(fileWithPath);
+
+        //Copy file at destination
+
+        FileUtils.copyFile(SrcFile, DestFile);
+
+    }
+
+}
