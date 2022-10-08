a81917b73196e994f4a218076e44bdf061cf9e9e
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 20:16:34 +0300

    task has been finalized

--- src/test/java/com/example/testing/HandleAuthenticatedPopUps.java
+++ src/test/java/com/example/testing/HandleAuthenticatedPopUps.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class HandleAuthenticatedPopUps {
+public class HandleAuthenticatedPopUps extends AbstractApplicationTest {
     String URL = "https://the-internet.herokuapp.com/basic_auth";
 
     @Test
