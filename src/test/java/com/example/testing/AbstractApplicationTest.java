ef485f2f97991318903b5c1706e40101ee04121f
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 18:38:08 +0300

    add screenshot method

--- src/test/java/com/example/testing/AbstractApplicationTest.java
+++ src/test/java/com/example/testing/AbstractApplicationTest.java
@@ -1,13 +1,19 @@
 package com.example.testing;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.io.File;
+
 public class  AbstractApplicationTest {
 
+    final String basePath = ".\\screenshot\\";
     WebDriver driver;
     @BeforeTest
     void setup() {
@@ -19,7 +25,21 @@ public class  AbstractApplicationTest {
     @AfterTest
     void cleanup() throws InterruptedException {
         Thread.sleep(2000L);
-        driver.close();
+        driver.quit();
+    }
+
+    public static void takeSnapShot(WebDriver webdriver,String fileName) throws Exception{
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+        //Move image file to new destination
+        File DestFile=new File(fileName);
+
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
     }
 
 }
