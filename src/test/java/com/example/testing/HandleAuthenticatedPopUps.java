321f8ea3472757dc08d8aa4ae02aa11f314fd36c
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 21:28:45 +0300

    useless imports have been deleted

--- src/test/java/com/example/testing/HandleAuthenticatedPopUps.java
+++ src/test/java/com/example/testing/HandleAuthenticatedPopUps.java
@@ -1,9 +1,5 @@
 package com.example.testing;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class HandleAuthenticatedPopUps extends AbstractApplicationTest {
@@ -11,9 +7,6 @@ public class HandleAuthenticatedPopUps extends AbstractApplicationTest {
 
     @Test
     void testCase3() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.get(URL);
         String a = "admin";
         String s = "https://" + a + ":" + a + "@" +
