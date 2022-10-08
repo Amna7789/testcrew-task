69dacd2c0d9a97c975ce73baa9feff92724e4d17
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 21:26:34 +0300

    useless imports have been deleted

--- src/test/java/com/example/testing/TestDropDownSorting.java
+++ src/test/java/com/example/testing/TestDropDownSorting.java
@@ -1,10 +1,7 @@
 package com.example.testing;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -14,9 +11,6 @@ public class TestDropDownSorting extends AbstractApplicationTest {
 
     @Test
     void testCase2() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
         driver.get(URL);
 
         driver.findElement(By.id("details-button")).click();
