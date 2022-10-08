a3aa5d5b75d8012490023525c3b39af580056f22
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 04:12:15 +0300

    still in progress..

--- src/test/java/com/example/testing/BootStrapDropDown.java
+++ src/test/java/com/example/testing/BootStrapDropDown.java
@@ -3,8 +3,12 @@ package com.example.testing;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BootStrapDropDown {
     String URL = "https://www.hdfcbank.com/";
@@ -16,7 +20,17 @@ public class BootStrapDropDown {
         driver.manage().window().maximize();
         driver.get(URL);
         Thread.sleep(5000L);
+        //select Product Type
+        List<WebElement> selectedList = driver.findElements(By.xpath("//h3[contains(., 'Select Product Type')]"));
+        Assert.assertNotEquals(0,selectedList.size(),"Result doesn't have Product Type");
+        selectedList.get(0).click();
+        //capture all items to list
+        List<WebElement> dropDownList = driver.findElements(By.xpath("//*[@class = 'sbct']"));
+        for (WebElement webElement : dropDownList){
+            System.out.println(webElement.getText());
+        }
         driver.close();
+
     }
 
 }
