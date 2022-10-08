2ef14fecc7701304a0564bbd2698671a2e2609e1
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 14:25:05 +0300

    task2 has done till LIVE POSTING

--- src/test/java/com/example/testing/TestDropDownSorting.java
+++ src/test/java/com/example/testing/TestDropDownSorting.java
@@ -3,8 +3,11 @@ package com.example.testing;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class TestDropDownSorting {
     String URL = "https://www.twoplugs.com/";
@@ -15,7 +18,20 @@ public class TestDropDownSorting {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL);
-        driver.findElement(By.linkText("LivePosting")).click();
+
+        driver.findElement(By.id("details-button")).sendKeys("Advanced");
+        List<WebElement> list = driver.findElements(By.xpath("//*[@class='secondary-button small-link']"));
+        driver.findElement(By.id("details-button")).click();
+
+        driver.findElement(By.id("details-button")).sendKeys("Hide advanced");
+        List<WebElement> list1 = driver.findElements(By.xpath("//*[@class='secondary-button small-link']"));
+        driver.findElement(By.id("details-button")).click();
+
+        driver.findElement(By.id("proceed-link")).sendKeys("Proceed to www.twoplugs.com (unsafe)");
+        List<WebElement> list2 = driver.findElements(By.xpath("//*[@class='small-link']"));
+        driver.findElement(By.id("proceed-link")).click();
+
+        driver.findElement(By.linkText("LIVE POSTING")).click();
         Thread.sleep(5000L);
         driver.close();
     }
