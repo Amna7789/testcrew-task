248eb0d3bd8e660d4b88f3c4ccd45266f5f972df
amna7789 <amnaaltaf242@gmail.com>
2022-10-08 13:30:26 +0300

    working on it

--- src/test/java/com/example/testing/HandleAutoCompleteGooglePlacesTest.java
+++ src/test/java/com/example/testing/HandleAutoCompleteGooglePlacesTest.java
@@ -3,8 +3,11 @@ package com.example.testing;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HandleAutoCompleteGooglePlacesTest {
 
@@ -16,7 +19,21 @@ public class HandleAutoCompleteGooglePlacesTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(URL);
-        driver.findElement(By.linkText("LivePosting")).click();
+        driver.findElement(By.id("details-button")).sendKeys("Advanced");
+
+        List<WebElement> list = driver.findElements(By.xpath("//*[@class='secondary-button small-link']"));
+        //driver.findElement(By.id("details-button")).submit();
+        driver.findElement(By.id("details-button")).click();
+
+        driver.findElement(By.id("details-button")).sendKeys("Hide advanced");
+        List<WebElement> list1 = driver.findElements(By.xpath("//*[@class='secondary-button small-link']"));
+        driver.findElement(By.id("details-button")).click();
+        driver.findElement(By.className("//*[@class='secondary-button small-link']")).submit();
+
+        driver.findElement(By.id("proceed-link")).click();
+        driver.findElement(By.id("proceed-link")).sendKeys("Proceed to www.twoplugs.com (unsafe)");
+        driver.findElement(By.className("//*[@class='small-link']")).submit();
+        //driver.findElement(By.linkText("Live Posting")).click();
         Thread.sleep(5000L);
         driver.close();
     }
