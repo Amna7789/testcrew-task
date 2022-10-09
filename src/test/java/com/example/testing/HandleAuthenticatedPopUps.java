637013fb90208350d153d8fec8a3b81a4055952e
amna <104275597+Amna7789@users.noreply.github.com>
2022-10-09 10:34:16 +0300

    Test handle authenticated pop ups (#13)
    * task 3 has been done scuccessfully
    
    * task finalized
    
    * task has been finalized
    
    * changes have been done successfully
    
    * useless imports have been deleted

---
+++ src/test/java/com/example/testing/HandleAuthenticatedPopUps.java
@@ -0,0 +1,21 @@
+package com.example.testing;
+
+import org.testng.annotations.Test;
+
+public class HandleAuthenticatedPopUps extends AbstractApplicationTest {
+    String URL = "https://the-internet.herokuapp.com/basic_auth";
+
+    @Test
+    void testCase3() throws InterruptedException {
+        driver.get(URL);
+        String a = "admin";
+        String s = "https://" + a + ":" + a + "@" +
+                "the-internet.herokuapp.com/basic_auth";
+
+        Thread.sleep(5000L);
+
+        driver.get(s);
+    
+    }
+
+}
