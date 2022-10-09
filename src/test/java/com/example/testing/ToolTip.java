d73554d89d43a538be962823791c0c7863efc2f4
amna <104275597+Amna7789@users.noreply.github.com>
2022-10-09 10:37:42 +0300

    Test capture tooltip (#10)
    * working on task
    
    * still in progress..
    
    * parent class has been added
    
    * did some changes
    
    * useless imports have been removed

---
+++ src/test/java/com/example/testing/ToolTip.java
@@ -0,0 +1,32 @@
+package com.example.testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class ToolTip extends AbstractApplicationTest{
+    String URL = "https://jqueryui.com/tooltip/";
+
+    @Test
+    void testCase4() throws InterruptedException {
+
+        driver.get(URL);
+
+        System.out.println("Tooltip web Page Displayed");
+        // Get element for which we need to find tooltip
+        WebElement ageTextBox = driver.findElement(By.id("age"));
+
+        //Get title attribute value
+        String tooltipText = ageTextBox.getAttribute("title");
+
+        System.out.println("Retrieved tooltip text as :" + tooltipText);
+
+        //Verification if tooltip text is matching expected value
+        if (tooltipText.equalsIgnoreCase("We ask for your age only for statistical purposes.")) {
+            System.out.println("Pass : Tooltip matching expected value");
+        } else {
+            System.out.println("Fail : Tooltip NOT matching expected value");
+        }
+
+    }
+}
